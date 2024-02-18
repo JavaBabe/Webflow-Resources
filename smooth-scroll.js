a0fdcc6eaa21c3d497675ebb97b761c0d32851bc
Toqeer Iqbal <toqeer.94@gmail.com>
2024-02-18 14:53:07 +0500

    Smooth Scroll

---
+++ smooth-scroll.js
@@ -0,0 +1,18 @@
+// Smooth Scroll - Add this script in footer
+// <script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@0.2.28/bundled/lenis.js"></script>
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: 'vertical', // vertical, horizontal
+  gestureDirection: 'vertical', // vertical, horizontal, both
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+})
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
